b765aed9b46d0dd95dc34f11fb25f5ad2bd26ed7
root <root@vps.server.local>
2022-03-04 18:21:02 +0000

    remoção de comentário

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# Flask: própria classe Flask | Response: para criar o retorno da API | request: tratar body enviado pelo cliente
 from controller.Account import *
 
-# para criar a tabela apenas uma vez, basta, via linha de comando, importar o db do app (from app import db) e executar db.create_all()
 app.run(host="0.0.0.0")
 
