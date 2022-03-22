905254b5313f272c95f6a9749e2d17738b2c0192
Adam Sori <adam.sori@gmail.com>
2022-03-22 01:35:26 -0300

    Remoção de comentários

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-''' 
-from flask import Flask
-# instanciar um app que vem do objeto flask, chamando o mesmo arquivo passando __name__
-app = Flask(__name__)
-
-# decorator do app com uma rota
-@app.route('/')
-def hello_world():
-    return "Hello World."
-'''
-
 from src.server.instance import server 
 # IMPORTAR OS CONTROLLERS (CLASSES)
 from src.controllers.transaction import *
